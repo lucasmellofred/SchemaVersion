9b15ed155645fe51e25abe78c8aebd3c5fd6ca68
Lucas Mello <lucasmello@univille.com>
2024-11-20 18:19:12 -0300

    Versionamento com Liquibase

---
+++ liquibase/SchemaVersionLiquibase/meutrabalholiquibase/src/main/java/gcso/versionamentodb/meutrabalholiquibase/MeutrabalholiquibaseApplication.java
@@ -0,0 +1,13 @@
+package gcso.versionamentodb.meutrabalholiquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeutrabalholiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeutrabalholiquibaseApplication.class, args);
+	}
+
+}
---
+++ liquibase/SchemaVersionLiquibase/meutrabalholiquibase/src/test/java/gcso/versionamentodb/meutrabalholiquibase/MeutrabalholiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package gcso.versionamentodb.meutrabalholiquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeutrabalholiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
